d30b656ab66e31c061702025648a69c8b4e59280
Asgarindo Dwiki Ibrahim adji <asgarindo9@gmail.com>
2024-05-29 14:58:43 +0700

    add project

--- src/app/components/project.js
+++ src/app/components/project.js
@@ -1,51 +1,42 @@
 import { FaExternalLinkAlt, FaCodeBranch } from "react-icons/fa";
+import Image from "next/image";
 
 const Project = () => {
   const projects = [
     {
-      title: "E-commerce Platform",
+      title: "asgarindoo.vercel.app",
       description:
-        "This project involves creating a full-stack web application for an e-commerce platform using Next.js and Tailwind CSS.",
+        "My personal portfolio website designed with a modern and elegant bento user interface (UI)",
       stack: ["Next.js", "Tailwind CSS"],
-      image: "https://via.placeholder.com/400",
-      demoLink: "https://example.com",
-      githubLink: "https://github.com",
+      image: "/projectImage/p4.png",
+      demoLink: "asgarindoo.vercel.app",
+      githubLink: "https://github.com/asgarindoo/Portofolio-v1",
     },
     {
-      title: "Mobile Task Manager",
-      description:
-        "This project focuses on building a mobile task manager application using React Native.",
-      stack: ["React Native"],
-      image: "https://via.placeholder.com/400",
-      demoLink: "https://example.com",
-      githubLink: "https://github.com",
+      title: "Glamour Gateway",
+      description: "International Beauty Product Delivery Service Website",
+      stack: ["MongoDB", "Express.js", "Vue.js", "Node.js", "Tailwind CSS"],
+      image: "/projectImage/p3.png",
+      demoLink: "",
+      githubLink: "https://github.com/asgarindoo/Glamour-Gateway",
     },
     {
-      title: "Stock Price Predictor",
+      title: "LIVING LOFT",
       description:
-        "This project is about developing a machine learning model to predict stock prices using Python and TensorFlow.",
-      stack: ["Python", "TensorFlow"],
-      image: "https://via.placeholder.com/400",
-      demoLink: "https://example.com",
-      githubLink: "https://github.com",
+        "An e-commerce platform that offers a wide range of furniture products in various styles and qualities.",
+      stack: ["HTML", "CSS", "JavaScript"],
+      image: "/projectImage/p2.png",
+      demoLink: "https://asgarindoo.github.io/SemanticHtml/",
+      githubLink: "https://github.com/asgarindoo/SemanticHtml",
     },
     {
-      title: "Corporate Website Redesign",
+      title: "Redesign Disbudpar Kabupaten Brebes",
       description:
-        "This project entails redesigning the corporate website of a tech company with a modern user interface.",
-      stack: ["UI/UX Design"],
-      image: "https://via.placeholder.com/400",
-      demoLink: "https://example.com",
-      githubLink: "https://github.com",
-    },
-    {
-      title: "Weather Forecasting API",
-      description:
-        "This project involves building a RESTful API for a weather forecasting application using Node.js and Express.",
-      stack: ["Node.js", "Express"],
-      image: "https://via.placeholder.com/400",
-      demoLink: "https://example.com",
-      githubLink: "https://github.com",
+        "The Redesign and Development of Disbudpar Kabupaten Brebes project aimed to modernize the official website of the Department of Culture and Tourism (Disbudpar) of Brebes Regency.",
+      stack: ["ASP.NET", "Razor Pages", "CSS", "JavaScript", "C#"],
+      image: "/projectImage/p1.png",
+      demoLink: "",
+      githubLink: "https://github.com/asgarindoo/Redesign-disbudpar",
     },
   ];
 
@@ -58,7 +49,7 @@ const Project = () => {
           <h2 className="text-2xl text-[#c4c4c4] font-manrope mb-4">
             {project.title}
           </h2>
-          <p className="text-[#a9a9a9] mb-2 pr-3 group-hover:max-w-[70%] transition-all ease-in-out duration-300">
+          <p className="text-[#a9a9a9] mb-2 pr-3 group-hover:max-w-[68%] transition-all ease-in-out duration-300">
             {project.description}
           </p>
           <div className="flex flex-wrap mb-4">
@@ -71,28 +62,29 @@ const Project = () => {
             ))}
           </div>
           <div className="absolute inset-y-0 right-0 w-1/3 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-            <img
+            <Image
               src={project.image}
               alt={project.title}
-              className="max-h-full max-w-full"
+              layout="fill"
+              className="max-h-full max-w-full object-cover"
             />
           </div>
           <div className="flex space-x-4 absolute bottom-4 left-4 text-sm">
             <a
-              href={project.demoLink}
-              className="text-[#c4c4c4] hover:underline flex items-center ml-5"
-              target="_blank"
-              rel="noopener noreferrer">
-              <FaExternalLinkAlt className="mr-1" />
-              Demo
-            </a>
-            <a
               href={project.githubLink}
-              className="text-[#c4c4c4] hover:underline flex items-center"
+              className="text-[#c4c4c4] hover:underline flex items-center ml-5"
               target="_blank"
               rel="noopener noreferrer">
               <FaCodeBranch className="mr-1" />
               GitHub
+            </a>
+            <a
+              href={project.demoLink}
+              className="text-[#c4c4c4] hover:underline flex items-center"
+              target="_blank"
+              rel="noopener noreferrer">
+              <FaExternalLinkAlt className="mr-1" />
+              Demo
             </a>
           </div>
         </div>
